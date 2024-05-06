1e03114889444abfc8ba6ce665f3a2d09dd85528
Shapirenko <158288545+Shapirenko@users.noreply.github.com>
2024-05-06 14:42:28 +0300

    Js

---
+++ market.js
@@ -0,0 +1,40 @@
+
+//перевірка підключеного файлу скриптів
+console.log('Перевірка підключеного файлу скрипту market.js')
+
+//Отримання елементу з ідентифікатором items
+let itemsDiv = document.getElementById("items")
+
+//Вивід знайдкного елементу
+// console.log(itemsDiv)
+
+//Перевірка існування знайденого блоку
+if (itemsDiv) {
+    //Вивід знайденого елементу
+    console.log(itemsDiv)
+
+    //Вивід значення поля знайденого елементу
+    // console.log('Поле classList:', itemsDiv.classList)
+    // console.log('Поле id:', itemsDiv.id)
+    // console.log('Поле clientWidth:', itemsDiv.clientWidth)
+    // console.log('Поле innerHTML:', itemsDiv.innerHTML)
+
+    //Додавання тексту в блок
+    // itemsDiv.innerHTML = 'Перший Програмно доданий текст'
+    // // itemsDiv.innerHTML = 'Другий Програмно доданий текст'
+    // itemsDiv.innerHTML += 'Другий Програмно доданий текст'
+    //Додавання відформатованого HTML коду в блок
+    // itemsDiv.innerHTML = '<div  class = item></div>'
+    // itemsDiv.innerHTML += '<div  class = "item"></div>'
+    // itemsDiv.innerHTML += '<div  class = "item"></div>'
+    // itemsDiv.innerHTML += '<div  class = "item"></div>'
+    // itemsDiv.innerHTML += '<div  class = "item"></div>'
+    for(let i = 0; i < 50; i++) {
+        itemsDiv.innerHTML += '<div  class = "row"><div class="item"></div><div class="item"></div></div>'
+        console.log('item')
+    }
+
+} else {
+    //Вивід знайденого елементу
+    console.log('Блок товарів не знайдено')
+}
